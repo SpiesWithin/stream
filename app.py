60c49d73b28efc35c9776bd64854bff67b70235a
Jay Nagpaul <jaynagpaul@gmail.com>
2017-02-24 13:31:17 -0500

    Added 'static'

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
-import flask
+import flask, itsdangerous
 from flask_recaptcha import ReCaptcha
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,8 +8,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route("/")
+def index():
+    return render_template("index.html")
 
-
+@app.route("/stats/<token>")
+def stats(token):
+    return render_template("main.html")
 
 
 if __name__ == '__main__':
